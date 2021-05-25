b656acdb8fa1e00b6d198830d4424b316762ba71
Jorge Neto <53627696+Jorge-Neto@users.noreply.github.com>
2021-05-25 20:57:33 -0300

    Adicionando POST
    Adicionando o POST ao projeto

--- JLG-bank/src/app/content/bank-accounts/register/register.component.ts
+++ JLG-bank/src/app/content/bank-accounts/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { BuscarClienteService } from 'src/app/services/buscarcliente.service';
+import { Usuario } from '../../dados/user';
 
 @Component({
   selector: 'app-register',
@@ -8,18 +11,26 @@ import { FormGroup } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
 
-  formulario: FormGroup;
+  // formulario: FormGroup;
+  // nomePlano = '';
+  // conta: string;
+  usuario: Usuario;
 
-  nomePlano = '';
-
-  conta: string;
+  constructor(private service: BuscarClienteService,
+    private router: Router) {
+    this.usuario = {} as Usuario;
+  }
 
   ngOnInit(): void {
     // this.formulario = this.busca.retornarForms();
   }
 
-  registrarUsuario() {
-    console.log();
+  registrarUsuario(usuario: Usuario) {
+    this.usuario.privilegio = "standard";
+    this.service.criarUsuario(usuario).then(() => {
+      console.log(`${usuario.nome} criado!`);
+      this.router.navigate(['/admin']);
+    })
   }
 
 }
